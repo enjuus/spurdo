3d2f259f1c590e78be95329a9523786d64125c30
enjuus <skr0tm@protonmail.com>
2019-08-15 17:55:03 +0200

    Changed regex

--- spurdo.go
+++ spurdo.go
@@ -1,12 +1,18 @@
 // a spurdo dranzlator paggage.
-package spurdo
+package main
 
 import (
 	"errors"
+	"fmt"
 	"math/rand"
 	"regexp"
 	"strings"
 )
+func main() {
+	string := "translate this faggot. test ."
+	str, _ := Translate(string)
+	fmt.Println(str)
+}
 
 // tagez a sdrign to be "dranzlated" idno spurdo
 func Translate(String string) (string, error) {
@@ -17,71 +23,71 @@ func Translate(String string) (string, error) {
 
 	replacements := map[string]string{
 		"america": "clapistan",
-		"meme":    "maymay",
-		"some":    "sum",
-		"epic":    "ebin",
-		"kek":     "geg",
-		"right":   "rite",
-		"ng":      "nk",
-		"ic":      "ig",
-		"ing":     "ign",
-		"alk":     "olk",
-		"ys":      "yz",
-		"ws":      "wz",
-		"us":      "uz",
-		"ts":      "tz",
-		"ss":      "sz",
-		"rs":      "rz",
-		"ns":      "nz",
-		"ms":      "mz",
-		"ls":      "lz",
-		"is":      "iz",
-		"gs":      "gz",
-		"fs":      "fz",
-		"es":      "ez",
-		"ds":      "dz",
-		"bs":      "bz",
-		"tr":      "dr",
-		"pr":      "br",
-		"nt":      "dn",
-		"mm":      "m",
-		"lt":      "ld",
-		"kn":      "gn",
-		"cr":      "gr",
-		"ck":      "gg",
-		"va":      "ba",
-		"up":      "ub",
-		"pi":      "bi",
-		"pe":      "be",
-		"po":      "bo",
-		"ot":      "od",
-		"op":      "ob",
-		"ke":      "ge",
-		"it":      "id",
-		"iv":      "ib",
-		"et":      "ed",
-		"ex":      "egz",
-		"ev":      "eb",
-		"co":      "go",
-		"cx":      "gg",
-		"ca":      "ga",
-		"ap":      "ab",
-		"af":      "ab",
-		"th":      "d",
-		"wh":      "w",
+		"meme": "maymay",
+		"some": "sum",
+		"epic": "ebin",
+		"kek": "geg",
+		"right": "rite",
+		"ng": "nk",
+		"ic": "ig",
+		"ing": "ign",
+		"alk": "olk",
+		"ys": "yz",
+		"ws": "wz",
+		"us": "uz",
+		"ts": "tz",
+		"ss": "sz",
+		"rs": "rz",
+		"ns": "nz",
+		"ms": "mz",
+		"ls": "lz",
+		"is": "iz",
+		"gs": "gz",
+		"fs": "fz",
+		"es": "ez",
+		"ds": "dz",
+		"bs": "bz",
+		"tr": "dr",
+		"pr": "br",
+		"nt": "dn",
+		"mm": "m",
+		"lt": "ld",
+		"kn": "gn",
+		"cr": "gr",
+		"ck": "gg",
+		"va": "ba",
+		"up": "ub",
+		"pi": "bi",
+		"pe": "be",
+		"po": "bo",
+		"ot": "od",
+		"op": "ob",
+		"ke": "ge",
+		"it": "id",
+		"iv": "ib",
+		"et": "ed",
+		"ex": "egz",
+		"ev": "eb",
+		"co": "go",
+		"cx": "gg",
+		"ca": "ga",
+		"ap": "ab",
+		"af": "ab",
+		"th": "d",
+		"wh": "w",
 	}
 
 	ebinfaces := map[int]string{
-		0:  ":D",
-		1:  ":DD",
-		2:  ":DDD",
-		3:  ":-D",
-		4:  "XD",
-		5:  "XXD",
-		6:  "XDD",
-		7:  "XXDD",
-		8:  "xD",
-		9:  "xDD",
+		0: ":D",
+		1: ":DD",
+		2: ":DDD",
+		3: ":-D",
+		4: "XD",
+		5: "XXD",
+		6: "XDD",
+		7: "XXDD",
+		8: "xD",
+		9: "xDD",
 		10: ":dd",
 		11: ":d",
 	}
@@ -91,10 +97,11 @@ func Translate(String string) (string, error) {
 	for search, replace := range replacements {
 		String = strings.ReplaceAll(String, search, replace)
 	}
-	r := regexp.MustCompile(",|\n.")
+	r := regexp.MustCompile(`[.,\/#!$%\^&\*;:]`)
 	matches := r.FindAllString(String, -1)
+	fmt.Println(matches)
 	for _, match := range matches {
-		String = strings.ReplaceAll(String, match, ebinfaces[rand.Intn(len(ebinfaces))])
+		String = strings.Replace(String, match, ebinfaces[rand.Intn(len(ebinfaces))], 1)
 	}
 
 	return String, nil
